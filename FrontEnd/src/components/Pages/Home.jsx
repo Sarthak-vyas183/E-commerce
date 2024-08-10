2d5a7e28776c860636e9b04ead659415f386eeb0
Sarthak vyas <142726436+Sarthak-vyas183@users.noreply.github.com>
2024-08-10 08:33:41 +0530

    home page scrollbar

--- FrontEnd/src/components/Pages/Home.jsx
+++ FrontEnd/src/components/Pages/Home.jsx
@@ -1,34 +1,72 @@
-import React, { useEffect } from 'react'
-import { useRef } from "react";
-import "../../../public/Stylesheets/home.css"
-import { gsap } from "gsap"; 
+import React, { useRef, useEffect } from 'react';
+import '../../../public/Stylesheets/home.css';
+import { gsap } from 'gsap';
 
-function Home() { 
-  const discountRef = useRef(null);
+function Home() {
+  const bannerRef = useRef(null);
 
-   useEffect(()=> {
-        gsap.from(discountRef.current , {
-           opacity : 0,
-           scale : 0,
-           duration : 0.8,
-           
-        })
-   },[])
+  useEffect(() => {
+    const banner = bannerRef.current;
+    const scrollAmount = window.innerWidth; // Scroll by the viewport width
+    const middleIndex = Math.floor(banner.children.length/2);
+    const initialOffset = -middleIndex * scrollAmount;
+
+    
+    gsap.set(banner, { x: initialOffset });
+  }, []);
+
+  const scrollBanner = (direction) => {
+    const banner = bannerRef.current;
+    const scrollAmount = window.innerWidth; 
+    const currentPosition = gsap.getProperty(banner, 'x');
+    const maxScrollLeft = 0;
+    const maxScrollRight = -(banner.scrollWidth - banner.clientWidth);
+
+    
+    let newPosition = direction === 'left' ? currentPosition + scrollAmount : currentPosition - scrollAmount;
+
+    
+    if (newPosition > maxScrollLeft) {
+      newPosition = maxScrollLeft;
+    } else if (newPosition < maxScrollRight) {
+      newPosition = maxScrollRight;
+    }
+
+   
+    gsap.to(banner, {
+      x: newPosition,
+      duration : 0.1,
+    });
+
+    gsap.from(banner, {
+       scale : 0,
+    });
+  };
 
   return (
-    <section ref={discountRef} id='home-main'  className='relative top-[11vh] w-[100vw] min-h-[89vh]  z-1 overflow-x-hidden '>
-         <div className='w-full h-auto'>
-          <img src="../../../TempImg/discount.png" alt="" />
-         </div>
-
-         <div id='banner-02' className='flex justify-center'>
-           <img className='w-full' src="../../../TempImg/discountBanner.png" alt="" />
-         </div> 
-
-         <div></div>
-         <div></div>
+    <section id='home-main' className='relative top-[11vh] w-[100vw] min-h-[89vh] z-1 overflow-x-hidden'> 
+      <div className='relative w-full h-[70vh]'>
+        <div id='banner-02' ref={bannerRef} className='flex transition-transform duration-500 ease-in-out'>
+          <img className='w-full h-full object-cover' src='https://richborn.s3.ap-south-1.amazonaws.com/banner2.jpg' alt='Discount Banner 1' />
+          <img className='w-full h-full object-cover' src='https://richborn.s3.ap-south-1.amazonaws.com/banner%203%20part%20b.jpg' alt='Discount Banner 2' />
+          <img className='w-full h-full object-cover' src='https://richborn.s3.ap-south-1.amazonaws.com/richborn-banner-bag.jpg' alt='Discount Banner 3' />
+          <img className='w-full h-full object-cover' src='https://richborn.s3.ap-south-1.amazonaws.com/richborn-banner-bag.jpg' alt='Discount Banner 4' />
+        </div>
+        <button 
+          className='absolute top-1/2 left-0 transform -translate-y-1/2 bg-black text-white p-2'
+          onClick={() => scrollBanner('left')}
+        >
+          &#8249;
+        </button>
+        <button 
+          className='absolute top-1/2 right-0 transform -translate-y-1/2 bg-black text-white p-2'
+          onClick={() => scrollBanner('right')}
+        >
+          &#8250;
+        </button>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Home
+export default Home;
